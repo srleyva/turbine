8f93420cc3dfc874fb064c8b44be5a83244fb3ed
Stephen Leyva <sleyva1297@gmail.com>
2018-07-26 15:18:57 -0400

    init the login service

---
+++ login-service/client/src/app/_services/authentication.service.ts
@@ -0,0 +1,40 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
+
+    apiRoot:string = '/auth';
+
+    login(user) {
+        let apiURL = `${this.apiRoot}/login`;
+        let body = JSON.stringify(user)
+        return this.http.post<any>(apiURL, body, httpOptions)
+            .map(user => {
+                console.log(user)
+                // login successful if there's a jwt token in the response
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+
+                return user;
+            })
+            .catch(e => {
+                console.log(e);
+                return Observable.throw(e.error.message);
+            });
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
---
+++ login-service/client/src/app/_services/user.service.ts
@@ -0,0 +1,33 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    // TODO: In k8s this will not be localhost but name of k8s service
+    apiRoot:string = '/auth';
+
+    getAll() {
+        let apiURL = `${this.apiRoot}/users`;
+        return this.http.get<User[]>(apiURL);
+    }
+
+    getById(id: number) {
+        return this.http.get('/api/users/' + id);
+    }
+
+    create(user: User) {
+        return this.http.post('/api/users', user);
+    }
+
+    update(user: User) {
+        return this.http.put('/api/users/' + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id);
+    }
+}
