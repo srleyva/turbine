81dcfcbc587eb9b60d01f911451b23ef016b7668
Stephen Leyva <sleyva1297@gmail.com>
2018-07-28 19:01:54 -0400

    Change default name of service
    *Allow name of user_service to be passed to cmdline

--- authentication-service/cmd/server/server.go
+++ authentication-service/cmd/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/micro/cli"
 	log "github.com/micro/go-log"
 	micro "github.com/micro/go-micro"
 	"github.com/sirupsen/logrus"
@@ -11,19 +12,28 @@ import (
 	micrologrus "github.com/tudurom/micro-logrus"
 )
 
+var (
+	// service to call
+	serviceName string
+)
+
 func main() {
 	golog := logrus.New()
 	golog.Formatter = logging.Formatter
 	logger := micrologrus.NewMicroLogrus(golog)
 	log.SetLogger(logger)
 	authService := micro.NewService(
-		micro.Name("authentication-service"),
+		micro.Name("go.micro.srv.authentication"),
 		micro.WrapHandler(logging.LogWrapper),
 	)
+	authService.Init(micro.Flags(cli.StringFlag{
+		Name:        "user_service",
+		Value:       "go.micro.srv.user",
+		Destination: &serviceName,
+	}))
 
-	userClient := userProto.NewUserService("user-service", authService.Client())
+	userClient := userProto.NewUserService(serviceName, authService.Client())
 
-	authService.Init()
 	proto.RegisterAuthenticationServiceHandler(authService.Server(), &auth.Service{userClient})
 
 	if err := authService.Run(); err != nil {
