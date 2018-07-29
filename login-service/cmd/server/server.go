28294707ae5431394ce0140d45f5904ac55b7397
Stephen Leyva <sl_leyva25@yahoo.com>
2018-07-29 00:51:53 -0400

    Server (#2)
    * server static site
    
    * create web server for login

--- login-service/cmd/server/server.go
+++ login-service/cmd/server/server.go
@@ -8,11 +8,13 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/srleyva/turbine/login-service/pkg/login"
+	"net/http"
 )
 
 var (
 	// service to call
 	serviceName string
+	appDir      string
 )
 
 func main() {
@@ -22,7 +24,12 @@ func main() {
 		Name:        "service_name",
 		Value:       "go.micro.srv.authentication",
 		Destination: &serviceName,
-	}))
+	},
+		cli.StringFlag{
+			Name:        "app_dir",
+			Value:       "/app/wwwroot",
+			Destination: &appDir,
+		}))
 	auth := authProto.NewAuthenticationService(serviceName, service.Client())
 
 	// Initialize Server
@@ -31,15 +38,19 @@ func main() {
 	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
 		SigningKey: []byte(login.Key),
 		Skipper: func(c echo.Context) bool {
-			if c.Path() == "/login" || c.Path() == "/register" {
+			if c.Path() == "/auth/login" || c.Path() == "/auth/register" || c.Path() == "/static/*" || c.Path() == "/" {
 				return true
 			}
 			return false
 		},
 	}))
 
+	fs := http.FileServer(http.Dir(appDir))
+	e.GET("/", echo.WrapHandler(fs))
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", fs)))
+
 	h := login.Handler{auth}
-	e.POST("/login", h.Login)
-	e.POST("/register", h.Register)
+	e.POST("/auth/login", h.Login)
+	e.POST("/auth/register", h.Register)
 	e.Logger.Fatal(e.Start(":1323"))
 }
